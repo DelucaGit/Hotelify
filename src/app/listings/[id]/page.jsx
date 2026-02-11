96b3128da9e31fb6e7a879e07bed68ef84a5a812
Marcel Deluca <andalusi.private@gmail.com>
2026-02-11 16:22:11 +0200

    Bypass optimization rules of Next.js to use placeholder images

--- src/app/listings/[id]/page.jsx
+++ src/app/listings/[id]/page.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {hotels} from "../../data/localdata";
+import Image from "next/image";
 
 const Page = async ({params}) => {
     const {id} = await params;
-    const hotel = hotels.find(h => h.id.toString() === id);
+    const hotel = hotels.find(h => h.id.toString() === id); // Because URL params will always be String
     return (
-        <div>
-            {hotel.name}
-        </div>
+        <main>
+            <Image
+                src={hotel.image}
+                alt={hotel.name}
+                width={600}
+                height={400}
+                suppressHydrationWarning={true}
+                unoptimized={true}
+            />
+
+        </main>
     )
 }
 export default Page
