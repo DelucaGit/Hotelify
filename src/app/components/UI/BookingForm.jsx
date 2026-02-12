7a33948a523830a1f842229d114fa0d26fb3a1fb
Marcel Deluca <andalusi.private@gmail.com>
2026-02-12 22:03:50 +0200

    Form basics

--- src/app/components/UI/BookingForm.jsx
+++ src/app/components/UI/BookingForm.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+"use client";
+import React, {useState} from 'react'
 
-const BookingForm = () => {
+const BookingForm =  (hotelData) => {
+
+    const {hotel} =  hotelData
+    const [days,setDays] = useState(1);
+    const [guests,setGuests] = useState(1);
+
+    const costPerNight = (hotel.price * guests) * days;
+
+    async function handleBooking(formData){
+
+        alert("Hej")
+    }
     return (
-        <div>BookingForm</div>
+        <>
+            <form action={handleBooking}>
+                <input
+                    type="text"
+                    placeholder={"First name"}
+                    name={"firstname"}
+                    className={"border border-gray-100 p-2 rounded-2xl"}
+                />
+                <input
+                    type="text"
+                    placeholder={"Last name"}
+                    name={"lastname"}
+                    className={"border border-gray-100 p-2 rounded-2xl"}
+
+                /><br/><br/>
+                <input
+                    type="text"
+                    placeholder={"Email"}
+                    name={"email"}
+                    className={"border border-gray-100 p-2 rounded-2xl"}
+                />
+                <input type="tel" placeholder={"Phone"} name={"phone"} className={"border border-gray-100 p-2 rounded-2xl"}/><br/>
+                <button
+                    className={"bg-red-800 text-white font-bold p-2 rounded-2xl cursor-pointer mt-5"}
+                    type={"submit"}
+                    >
+                        Submit
+                </button>
+            </form>
+        </>
     )
 }
 export default BookingForm
